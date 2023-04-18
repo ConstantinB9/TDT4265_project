276ef6acf8950b7186d897b25d950754405616ce
constantinb9 <constantin.braess@gmail.com>
2023-04-18 11:52:16 +0000

    add weighted random sampler

--- yolo/trainer.py
+++ yolo/trainer.py
@@ -6,7 +6,7 @@ import random
 import numpy as np
 import torch
 from dataset import RDDDataset
-from torch.utils.data import DataLoader, distributed
+from torch.utils.data import DataLoader, distributed, WeightedRandomSampler
 from ultralytics import yolo
 from ultralytics.yolo.cfg import get_cfg
 from ultralytics.yolo.utils import (DEFAULT_CFG, RANK, SETTINGS, __version__,
@@ -16,6 +16,7 @@ from ultralytics.yolo.utils.files import increment_path
 from ultralytics.yolo.utils.torch_utils import (de_parallel, init_seeds,
                                                 select_device)
 from val import CustomValidator
+
 
 
 def seed_worker(worker_id):  # noqa
@@ -137,19 +138,19 @@ class CustomTrainer(yolo.v8.detect.DetectionTrainer):
             [os.cpu_count() // max(nd, 1), batch_size if batch_size > 1 else 0, workers]
         )  # number of workers
 
-        sampler = (
-            None
-            if rank == -1
-            else distributed.DistributedSampler(dataset, shuffle=shuffle)
-        )
-
         # sampler = (
-        #     WeightedRandomSampler(
-        #         weights=dataset.get_weights(), num_samples=len(dataset)
-        #     )
-        #     if mode == "train"
-        #     else None
+        #     None
+        #     if rank == -1
+        #     else distributed.DistributedSampler(dataset, shuffle=shuffle)
         # )
+
+        sampler = (
+            WeightedRandomSampler(
+                weights=dataset.get_weights(), num_samples=len(dataset)
+            )
+            if mode == "train"
+            else None
+        )
 
         loader = DataLoader
         generator = torch.Generator()
